8858b95799280005ea05ade194ab2e242a8dd7b4
Ko <orangeko@gmail.com>
2021-03-10 01:52:57 +0900

    3rd week Q2 processing, createElement appendChild

--- 3rd week Q1.py
+++ 3rd week Q1.py
@@ -1,9 +1,9 @@
 targetUrl = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json"
-src = ""
-# target_stitle
-# target_longitude
-# target_latitude
-# target_file
+
+# target stitle
+# target longitude
+# target latitude
+# target file
 
 import urllib.request as request
 import json
@@ -18,7 +18,7 @@ with open("data.txt", "w", encoding= "utf-8") as file:
     for target in target_list:
         proto_url = target["file"]
 
-        # 不知道為何split後的[0]沒有資料，但[1]就抓到正確資料
+        # 不知道為何split後的[0]沒有資料，但[1]就抓到正確資料 ← 想通了，在split http的狀況下，因為一開始就是http，所以[0]就變為空白內容
         first_url = proto_url.split("http://")[1]
         first_url_complete = "http://" + first_url
 
